b27413e538f7355593a483864265ca8c0396fcf3
Brittany Russell <sincerelybr@gmail.com>
2020-02-23 19:23:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-// add solution here
-function theBeatlesPlay(musicians, instruments) {
-  var plays = [];
-  for(var i = 0; musicians.length > i; i++) {
-    plays.push(musicians[i] + " plays " + instruments[i]);
-  }
-  return plays;
-}
+// // add solution here
+// function theBeatlesPlay(musicians, instruments) {
+//   var plays = [];
+//   for(var i = 0; musicians.length > i; i++) {
+//     plays.push(musicians[i] + " plays " + instruments[i]);
+//   }
+//   return plays;
+// }
 
-function johnLennonFacts(arg) {
-  var newFacts = [];
-  var len = arg.length;
-  var i = 0;
-  while(i < len) {
-  newFacts.push(arg[i] + "!!!");
-  i++;
-}
-  return newFacts;
-}
+// function johnLennonFacts(arg) {
+//   var newFacts = [];
+//   var len = arg.length;
+//   var i = 0;
+//   while(i < len) {
+//   newFacts.push(arg[i] + "!!!");
+//   i++;
+// }
+//   return newFacts;
+// }
+
+// function iLoveTheBeatles(arg) {
+//   var newFacts = [];
+//   var len = arg.length;
+//   var i = 0;
+//   while(i < len) {
+//   newFacts.push(arg[i] + "!!!");
+//   i++;
+// }
+//   return newFacts;
+// }
