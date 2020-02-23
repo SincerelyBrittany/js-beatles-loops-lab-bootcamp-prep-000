bc9e3db17991eb2e855489095229cb3a95f91819
Brittany Russell <sincerelybr@gmail.com>
2020-02-23 19:21:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var plays = [];
+  for(var i = 0; musicians.length > i; i++) {
+    plays.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return plays;
+}
+
+function johnLennonFacts(arg) {
+  var newFacts = [];
+  var len = arg.length;
+  var i = 0;
+  while(i < len) {
+  newFacts.push(arg[i] + "!!!");
+  i++;
+}
+  return newFacts;
+}
