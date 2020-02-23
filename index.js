242437b26fd76f7a2c93ee9850561505085d3a48
Brittany Russell <sincerelybr@gmail.com>
2020-02-23 19:58:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,26 +29,59 @@
 //   return newFacts;
 // }
 
-function theBeatlesPlay (musicians,instruments){
-  var songs =[];
-  for (var i = 0; i < musicians.length; i++){
-    songs.push(musicians[i] + " plays " + instruments[i])
-    console.log(musicians, "this is music");
-    console.log(instruments, "this is instrument")
-  }
-  return songs;
-}
+// function theBeatlesPlay (musicians,instruments){
+//   var songs =[];
+//   for (var i = 0; i < musicians.length; i++){
+//     songs.push(musicians[i] + " plays " + instruments[i])
+//     console.log(musicians, "this is music");
+//     console.log(instruments, "this is instrument")
+//   }
+//   return songs;
+// }
 
 
-function johnLennonFacts(arg){
-  fact = [];
-  while (i < 10) {
-  text += "The number is " + i;
-  i++;
-}
+// function johnLennonFacts(arg){
+//   fact = [];
+//   while (i < 10) {
+//   text += "The number is " + i;
+//   i++;
+// }
   
+// }
+
+
+function theBeatlesPlay(musicians, instruments) {
+  const arr = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return arr
 }
 
+function johnLennonFacts(facts) {
+  const shoutedFacts = []
+
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return shoutedFacts
+}
+
+function iLoveTheBeatles(n) {
+  const arr = []
+
+  do {
+    arr.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+
+  return arr
+}
 
 
 
