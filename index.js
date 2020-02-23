60cc657e8acbcd856a8ac66866ec7860b8c632e9
Brittany Russell <sincerelybr@gmail.com>
2020-02-23 19:41:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,10 +31,10 @@
 
 function theBeatlesPlay (musicians,instruments){
   var songs =[];
-  for (i = 0; i < musicians.length; i++){
+  for (var i = 0; i < musicians.length; i++){
     songs.push(musicians[i] + " plays " + instruments[i])
-    // console.log(musicians, "this is music");
-    // console.log(instruments, "this is instrument")
+    console.log(musicians, "this is music");
+    console.log(instruments, "this is instrument")
   }
   return songs;
 }
